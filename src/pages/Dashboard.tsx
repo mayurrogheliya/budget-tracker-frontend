c3bf01cf09a0878489a659683de3870498575e28
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-24 18:43:23 +0530

    import the Transaction Form and Transaction Table into main file

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,46 @@
 import React from "react";
+import TransactionForm from "../components/TransactionForm";
+import { Button, Flex, Space, Typography } from "antd";
+import { EyeOutlined, PlusOutlined } from "@ant-design/icons";
+import { useTransactionStore } from "../store/useTransactionStore";
+import TransactionTable from "../components/TransactionTable";
+
+const { Title } = Typography;
 
 const Dashboard: React.FC = () => {
+  const { mode, setMode } = useTransactionStore();
   return (
-    <div>
-      <h1>Hello Home page</h1>
-    </div>
+    <Space direction="vertical" style={{ width: "100%" }} className="mt-5">
+      <Flex justify="space-between">
+        <Title level={2}>Transaction</Title>
+        <>
+          {(mode == "form" || mode == "read-only-form") && (
+            <Button
+              type="primary"
+              icon={<EyeOutlined />}
+              onClick={() => {
+                setMode("table");
+              }}
+            >
+              {"View"}
+            </Button>
+          )}
+          {mode == "table" && (
+            <Button
+              type="primary"
+              icon={<PlusOutlined />}
+              onClick={() => {
+                setMode("form");
+              }}
+            >
+              {"Add"}
+            </Button>
+          )}
+        </>
+      </Flex>
+      {(mode == "form" || mode == "read-only-form") && <TransactionForm />}
+      {mode == "table" && <TransactionTable />}
+    </Space>
   );
 };
 
