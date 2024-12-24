267d8428960f8772b4e46be35dc6afc11151a57d
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-24 23:59:40 +0530

    feat: add static analytics pie chart

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,10 +1,52 @@
+import { Flex, Space, Typography } from "antd";
 import React from "react";
+
+const { Title } = Typography;
+
+import {
+  CategoryScale,
+  LinearScale,
+  ArcElement,
+  Tooltip as ChartTooltip,
+  Legend,
+  Chart as ChartJS,
+} from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+// Register all necessary Chart.js components
+ChartJS.register(CategoryScale, LinearScale, ArcElement, ChartTooltip, Legend);
+
+const chartData = {
+  labels: ["Income", "Expense"],
+  datasets: [
+    {
+      label: "Amount",
+      data: [1000, 500],
+      backgroundColor: ["#2ecc71", "#e74c3c"],
+    },
+  ],
+  hoverOffset: 4,
+};
 
 const Analytics: React.FC = () => {
   return (
-    <div>
-      <h1>Budget Analytics</h1>
-    </div>
+    <Space direction="vertical" style={{ width: "100%" }} className="mt-5">
+      <Flex justify="start">
+        <Title level={2}>Analytics</Title>
+        <>
+          <div
+            style={{
+              marginBottom: 16,
+              width: "300px",
+              height: "300px",
+              marginTop: "80px",
+            }}
+          >
+            <Pie data={chartData} options={{ responsive: true }} />
+          </div>
+        </>
+      </Flex>
+    </Space>
   );
 };
 
