9720510d97cae9d6923279a381d5bf767c72bfd4
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-25 16:40:53 +0530

    intergrates AnalyticsCard for displaying the income and expense in pie chart

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,4 +1,4 @@
-import { Flex, Space, Typography } from "antd";
+import { Space, Typography } from "antd";
 import React from "react";
 
 const { Title } = Typography;
@@ -11,7 +11,7 @@ import {
   Legend,
   Chart as ChartJS,
 } from "chart.js";
-import { Pie } from "react-chartjs-2";
+import AnalyticsCard from "../components/AnalyticsCard";
 
 // Register all necessary Chart.js components
 ChartJS.register(CategoryScale, LinearScale, ArcElement, ChartTooltip, Legend);
@@ -28,24 +28,54 @@ const chartData = {
   hoverOffset: 4,
 };
 
+const incomeData = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      label: "Income",
+      data: [65, 59, 80, 81, 56, 55],
+      backgroundColor: ["#2ecc71"],
+    },
+  ],
+  hoverOffset: 4,
+};
+
+const expenseData = {
+  labels: ["January", "February", "March", "April", "May", "June"],
+  datasets: [
+    {
+      label: "Expense",
+      data: [28, 48, 40, 19, 86, 27],
+      backgroundColor: ["#e74c3c"],
+    },
+  ],
+  hoverOffset: 4,
+};
+
 const Analytics: React.FC = () => {
   return (
     <Space direction="vertical" style={{ width: "100%" }} className="mt-5">
-      <Flex justify="start">
-        <Title level={2}>Analytics</Title>
-        <>
-          <div
-            style={{
-              marginBottom: 16,
-              width: "300px",
-              height: "300px",
-              marginTop: "80px",
-            }}
-          >
-            <Pie data={chartData} options={{ responsive: true }} />
-          </div>
-        </>
-      </Flex>
+      <Title level={2}>Analytics</Title>
+      <div className="flex justify-center items-center flex-wrap gap-10">
+        <AnalyticsCard
+          title="Income"
+          amount={200}
+          type="income"
+          chartData={incomeData}
+        />
+        <AnalyticsCard
+          title="Expense"
+          amount={500}
+          type="expense"
+          chartData={expenseData}
+        />
+        <AnalyticsCard
+          title="Total Income vs Expense"
+          amount={700}
+          type="total"
+          chartData={chartData}
+        />
+      </div>
     </Space>
   );
 };
