abac3ca284625131fba313b82c9e6efe393f8a33
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-27 19:41:26 +0530

    feat: add more transaction state to manage the transaction

--- src/store/useTransactionStore.tsx
+++ src/store/useTransactionStore.tsx
@@ -1,11 +1,43 @@
 import { create } from "zustand";
+import { transactionAPI } from "../api/endpoints/transaction";
+
+interface Transaction {
+  _id: string;
+  type: string;
+  amount: number;
+  description: string;
+  date: string;
+}
 
 interface TransactionState {
   mode: string;
   setMode: (mode: string) => void;
+  loading: boolean;
+  setLoading: (loading: boolean) => void;
+  transactions: Transaction[];
+  setTransactions: (transactions: Transaction[]) => void;
+  editingTransactions: Transaction | null;
+  setEditingTransactions: (transaction: Transaction | null) => void;
+  fetchTransactions: () => void;
 }
 
-export const useTransactionStore = create<TransactionState>((set) => ({
+export const useTransactionStore = create<TransactionState>((set, get) => ({
   mode: "table",
   setMode: (mode: string) => set({ mode }),
+  loading: false,
+  setLoading: (loading) => set({ loading }),
+  transactions: [],
+  setTransactions: (transactions) => set({ transactions }),
+  editingTransactions: null,
+  setEditingTransactions: (transaction) =>
+    set({ editingTransactions: transaction }),
+
+  fetchTransactions: async () => {
+    try {
+      const response = await transactionAPI.getAll();
+      get().setTransactions(response.data.data);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  },
 }));
