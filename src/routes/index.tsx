035ef1793a1e0b3873121474e1bfa29709a81f5f
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-02 13:19:58 +0530

    feat: create a route for register user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,11 +10,13 @@ import Analytics from "../pages/Analytics";
 import LoginForm from "../pages/LoginForm";
 import RootLayout from "../layout/RootLayout";
 import ProtectedRoute from "./ProtectedRoute";
+import RegisterForm from "../pages/RegisterForm";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route path="login" element={<LoginForm />} />
+      <Route path="register" element={<RegisterForm />} />
       <Route element={<ProtectedRoute />}>
         <Route element={<RootLayout />}>
           <Route index element={<Dashboard />} />
