82592cf55c7572a4b6e195f216e681bc34c88631
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-02 08:55:49 +0530

    chore: integrate ProtetedRoute and LoginForm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,14 +7,19 @@ import {
 import React from "react";
 import Dashboard from "../pages/Dashboard";
 import Analytics from "../pages/Analytics";
+import LoginForm from "../pages/LoginForm";
 import RootLayout from "../layout/RootLayout";
+import ProtectedRoute from "./ProtectedRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/">
-      <Route element={<RootLayout />}>
-        <Route path="" element={<Dashboard />} />
-        <Route path="/analytics" element={<Analytics />} />
+      <Route path="login" element={<LoginForm />} />
+      <Route element={<ProtectedRoute />}>
+        <Route element={<RootLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="/analytics" element={<Analytics />} />
+        </Route>
       </Route>
     </Route>
   )
