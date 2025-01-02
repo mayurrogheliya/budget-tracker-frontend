086be512b550c7096c1f3fe2dcfcac9203fc1e1f
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-02 08:51:41 +0530

    feat: add Axios interceptors for request authorization and response handling

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { useUserStore } from "../store/useUserStore";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -8,4 +9,30 @@ export const api: AxiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    console.log(error.response);
+    const { logout } = useUserStore.getState();
+    if (error.response && error.response.status === 401) {
+      logout();
+    }
+
+    return Promise.reject(error);
+  }
+);
